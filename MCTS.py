c4cf290b7cfa2329f919dd9dc318f10b0299d9d1
lucasbeto <skybilobran@gmail.com>
2021-07-20 01:31:33 -0300

    intial class/method structuring defn

--- MCTS.py
+++ MCTS.py
@@ -1,1 +1,24 @@
 ## Monte Carlos Tree Search Class
+
+class NodeMCTS:
+    def __init__(self, state, nnet, is_root=False, parent=None):
+        # self.edge = {n(s, a), q(s, a), p(s, a) = [], node = None}
+        self.is_root = is_root
+        self.parent = parent
+        self.state = state
+        self.selected_action = None
+
+    def select(self):
+
+    def expand(self):
+
+    def backup(self):
+
+
+
+class MCTS:
+    def __init__(self, state):
+        root = NodeMCTS(state, True)
+
+    def play(self, temperature):
+        s = root(state)
--- agent.py
+++ agent.py
@@ -1,1 +1,20 @@
 ## Agent
+
+class Agent:
+    def self_play(self):
+        """ Cria um training set. Em cada movimento, guardar:
+                1. o estado do jogo
+                2. as search probabilities (do MCTS)
+                3. o value (quando o jogo acaba)
+        """
+
+    def retrain_network(self):
+        """ Usando o training set criado em self_play, otimiza os pesos da rede neural (model.py)
+            Em um loop, retreinar de uma forma mais ou menos actor-critic ocm a funcao
+            l = (z-v)^2 + pi*log(p) + c*mod(theta)^2
+        """
+
+    def evaluate_network(self):
+        """" Teste para ver se a rede resultado de retrain_network eh melhor que a ultima.
+             Jogar x jogos entre as duas e substituir a melhor pela ganhadora.
+        """
